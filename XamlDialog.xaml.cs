dc3feaf6b22462507bfc55bf9a047d63d4d43cfa
Utkarsh Shigihalli <onlyutkarsh@gmail.com>
2016-01-26 20:54:17 +0000

    Initial commit

---
+++ XamlDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace XamlDialogInVSExtensionDemo
+{
+    /// <summary>
+    /// Interaction logic for XamlDialog.xaml
+    /// </summary>
+    public partial class XamlDialog : Window
+    {
+        public XamlDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
